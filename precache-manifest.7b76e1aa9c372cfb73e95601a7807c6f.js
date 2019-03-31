5d25fd526568f53519b2dc2da2f68442364e40d9
Ricardo Agra <ricardo@labtestanalyzer.com>
2019-03-31 11:23:12 +0100

    Updates

---
+++ precache-manifest.7b76e1aa9c372cfb73e95601a7807c6f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7631c0b29d031e6e8cfd1be4ab2360e",
+    "url": "/tamagotchi/static/media/happy.e7631c0b.svg"
+  },
+  {
+    "revision": "906b122c2d4ba1aa5ac8",
+    "url": "/tamagotchi/static/css/main.117a6078.chunk.css"
+  },
+  {
+    "revision": "c990ab4b20012272550d",
+    "url": "/tamagotchi/static/js/runtime~main.fdd2acfc.js"
+  },
+  {
+    "revision": "de2dd2f4bd1be10ecfb2",
+    "url": "/tamagotchi/static/js/2.5fef2b58.chunk.js"
+  },
+  {
+    "revision": "4d39e029bd08dafbff7a77d2c41be97d",
+    "url": "/tamagotchi/static/media/neutral.4d39e029.svg"
+  },
+  {
+    "revision": "280eb14163ab91cf2589c4b7ce40a487",
+    "url": "/tamagotchi/static/media/logo.280eb141.svg"
+  },
+  {
+    "revision": "1949bc130b019568a1e974d4752708d9",
+    "url": "/tamagotchi/static/media/super-happy.1949bc13.svg"
+  },
+  {
+    "revision": "906b122c2d4ba1aa5ac8",
+    "url": "/tamagotchi/static/js/main.a0bd7c09.chunk.js"
+  },
+  {
+    "revision": "3e789fab9116f87bd0b267b21264d116",
+    "url": "/tamagotchi/static/media/smiling.3e789fab.svg"
+  },
+  {
+    "revision": "44bda2f39bffc90b5dce789311e24666",
+    "url": "/tamagotchi/static/media/sad.44bda2f3.svg"
+  },
+  {
+    "revision": "a8fc337185ce3e123cff8426cce43545",
+    "url": "/tamagotchi/static/media/crying.a8fc3371.svg"
+  },
+  {
+    "revision": "36612b792bb2d3e93a51f5a3efcabc81",
+    "url": "/tamagotchi/static/media/sick.36612b79.svg"
+  },
+  {
+    "revision": "02f11d1f91bda442b86a5763de986296",
+    "url": "/tamagotchi/static/media/sunny.02f11d1f.svg"
+  },
+  {
+    "revision": "5039864cceefa1c9cf8f8a3aebbbd405",
+    "url": "/tamagotchi/static/media/moon.5039864c.svg"
+  },
+  {
+    "revision": "350cb3123c704442f26100ae8e825b22",
+    "url": "/tamagotchi/index.html"
+  }
+];
